2d9c55c33e1ba6461936a00c078117029552de81
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-10-14 02:29:13 -0400

    database seeds

--- data/migrations/20211014021648_users_table.js
+++ data/migrations/20211014021648_users_table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
-};
+    return knex.shema
+
+.createTable('users', tbl => {
+        tbl.increments('user_id')
+        tbl.text('username', 64).unique().notNullable()
+        
+        tbl.text('password',128).notNullable()
+        })
+        };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+  .dropTableIfExists('users')
 };
---
+++ data/migrations/20211014022406_classes_table.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return knex.schema    
+  
+  .createTable('classes',tbl =>{
+      tbl.increments('class_id').primary().unique()
+      tbl.text('className',64).notNullable()
+    tbl.text('classType',64)
+    tbl.string('startTime').notNullable()
+    tbl.string('duration').notNullable()
+    tbl.string('intensityLevel').notNullable()
+    tbl.string('location').notNullable()
+    tbl.integer('registeredAttendees')
+    tbl.integer('classMAX')
+
+  })
+
+
+};
+
+exports.down = function(knex) {
+  return knex.schema
+};
