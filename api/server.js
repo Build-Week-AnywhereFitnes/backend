a25032ab4cfbdadcbb3d0fc3b224d07767ded10f
Victoria Trac <victoriatrac@gmail.com>
2021-10-14 14:47:42 -0600

    added package.json and required deps in server.js

--- api/server.js
+++ api/server.js
@@ -1,17 +1,25 @@
 const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
 const cors = require('cors')
 const server = express()
 // const classRouter = require('')
 // const authRouter = require('')   
 
-server.use(express.json())  
-server.use(cors())  
+require('colors')
+
+server.use(express.json())
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
+
+const currentTime = new Date().toLocaleTimeString
 
 // server.use('/api/classes', classRouter)
 // server.use('/api/auth', authRouter)
 
 server.get('/', (req, res)=>{
-    res.send(`<h1>Server is working </h1>`)
+    res.send(`<h1>Server is working at ${currentTime}</h1>`)
 })
 
 module.exports = server
