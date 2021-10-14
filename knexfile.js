69a2615095fc08d2d4640c7c9aaae270c20eefad
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-10-13 22:20:29 -0400

    knexfile

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+const sharedConfig = {
+    client:'sqlite3',
+    useNullAsDefault: true,
+
+migrations:{
+    directory:'./data/migrations',
+},
+seeds:{
+    directory:'./data/seeds',
+},
+pool:{ afterCreate:(conn, done)=>{conn.run('PRAGMA foreign_keys = ON', done)},},
+}
+
+
+module.exports = {
+    development:{
+        ...sharedConfig, 
+        connection: {filename:'./data/anywherefitness.db3'}
+    },
+
+testing:{
+    ...sharedConfig, connection:{filename:'./data/anywherefitness.db3'}
+},
+
+production:{...sharedConfig,
+    connection:{filename:'./data/anywherefitness.db3'}
+}
+
+
+}
+
