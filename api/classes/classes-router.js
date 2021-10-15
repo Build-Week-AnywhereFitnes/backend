c0e703297c0f35c6c893015f4d017682fbc44cc3
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-10-14 20:40:46 -0700

    setting up classes-router

---
+++ api/classes/classes-router.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Classes = require('./classes-model');
+const router = express.Router();
+
+// [GET] All Classes
+
+router.get('/', (req, res, next) => {
+
+})
+
+// [GET] Class by Class_Id
+
+
+
+// [Post] Add New Class by Class_Id
+
+
+
+// [PUT] Update by Class_Id
+
+
+
+// [Del] Remove Class by Class_Id
+
+
+
+
+module.exports = router;
