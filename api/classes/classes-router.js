d9a58e6129f880736e21334b62ad02bcd650513b
Victoria Trac <victoriatrac@gmail.com>
2021-10-20 18:22:40 -0600

    got counter to work

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -5,7 +5,6 @@ const Classes = require('./classes-model')
 
 const restricted = require('../middleware/auth-middleware')
 const adminCheck = require('../middleware/admin-middleware');
-// const { default: knex } = require('knex');
 
 // getAllClasses()
 router.get('/', restricted, (req, res, next) => {
@@ -40,27 +39,17 @@ router.get('/:id', restricted, (req, res, next) => {
 router.get('/register/:id', async (req, res, next) => {
   const { id } = req.params
 
-  Classes.countTakenSpots(id)
-    .then((numberOfSpots) => {
-      const freeSpots = numberOfSpots[0].count
-      // return freeSpots
-      res.status(200).json(freeSpots)
-    })
-    .catch((err) => {
-      err.message = `Server failed`
-      next(err)
-    })
+  try {
+    const numSpots = await Classes.countTakenSpots(id)
+    const mSpots = await Classes.countMaxSpots(id)
+    const openSpots = mSpots[0].classMax - numSpots[0].count
+    const theClass = await Classes.getClassByClassId(id)
 
-  // Classes.countMaxSpots(id)
-  //   .then((spots) => {
-  //     // const spots = maxSpots[0].classMax
-  //     // return spots
-  //     res.status(200).json(spots)
-  //   })
-  //   .catch((err) => {
-  //     err.message = `counting max failed`
-  //     next(err)
-  //   })
+    res.status(200).json(`There are ${openSpots} out of ${mSpots[0].classMax} available for ${theClass[0].className}`)
+  } catch (err) {
+    err.message = `Server failed to find the number of ${openSpots} for ${theClass[0].className}`
+    next(err)
+  }
 })
 
 // // searchClasses(key)
